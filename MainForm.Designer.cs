a3658c2a105e694fee19b6a212f7c4fcdbe4cf0f
spiraspira <zabornik5350@gmail.com>
2024-12-25 20:13:50 +0300

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace SpurGearCalculation
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			SuspendLayout();
+			// 
+			// MainForm
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			FormBorderStyle = FormBorderStyle.FixedSingle;
+			MaximizeBox = false;
+			MinimizeBox = false;
+			Name = "MainForm";
+			StartPosition = FormStartPosition.CenterScreen;
+			Text = "Рассчет прямозубой цилиндрической зубчатой передачи";
+			ResumeLayout(false);
+		}
+
+		#endregion
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,10 @@
+namespace SpurGearCalculation
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
