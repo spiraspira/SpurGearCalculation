bda977fc92ec0171ec54a4f232ebc8d3658e1fb7
spiraspira <zabornik5350@gmail.com>
2024-12-26 16:42:26 +0300

    Add SpurGearPart class

---
+++ SpurGearCalculation.API/Classes/SpurGearPart.cs
@@ -0,0 +1,17 @@
+﻿namespace SpurGearCalculation.API.Classes;
+
+/// <summary>
+/// Часть цилиндрической передачи (шестерня / колесо).
+/// </summary>
+public class SpurGearPart
+{
+	public string Name { get; set; }
+
+	public SteelType SteelType { get; set; }
+
+	public ProcessingType ProcessingType { get; set; }
+
+	public double n { get; set; }
+
+	public double t { get; set; }
+}
