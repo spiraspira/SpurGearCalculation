3d707bbe799280af9a947807dd1ed960dc79e6a5
spiraspira <zabornik5350@gmail.com>
2024-12-28 00:53:54 +0300

    Add ModulusCoefficientApproximateValuesTable

---
+++ SpurGearCalculation.API/Tables/ModulusCoefficientApproximateValuesTable.cs
@@ -0,0 +1,13 @@
+﻿namespace SpurGearCalculation.API.Tables;
+
+public static class ModulusCoefficientApproximateValuesTable
+{
+	public static List<ModulusCoefficientApproximateValue> ModulusCoefficientApproximateValues { get; set; } = [
+		new ModulusCoefficientApproximateValue(TransmissionType.HeavyDutyPrecision, true, (45.0, 30.0)),
+		new ModulusCoefficientApproximateValue(TransmissionType.HeavyDutyPrecision, true, (30.0, 20.0)),
+		new ModulusCoefficientApproximateValue(TransmissionType.RegularGear,        true, (30.0, 20.0)),
+		new ModulusCoefficientApproximateValue(TransmissionType.RegularGear,        true, (20.0, 15.0)),
+		new ModulusCoefficientApproximateValue(TransmissionType.Rough,              true, (15.0, 10.0)),
+		new ModulusCoefficientApproximateValue(TransmissionType.Rough,              true, (15.0, 10.0)),
+	];
+}
