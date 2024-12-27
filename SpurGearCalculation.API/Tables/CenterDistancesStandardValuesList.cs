10fc127a1ad2e7386caf47622d64aafbb71aa858
spiraspira <zabornik5350@gmail.com>
2024-12-28 01:40:57 +0300

    Add CenterDistancesStandardValues

---
+++ SpurGearCalculation.API/Tables/CenterDistancesStandardValuesList.cs
@@ -0,0 +1,6 @@
+﻿namespace SpurGearCalculation.API.Tables;
+
+public static class CenterDistancesStandardValuesList
+{
+	public static List<double> CenterDistancesStandardValues { get; set; } = [40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400];
+}
